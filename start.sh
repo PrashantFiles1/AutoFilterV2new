94882614cd6fa21e7907598c807c5e753a01982f
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-31 08:00:55 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/PrashantFiles1/extrafilter /extrafilter
-cd /extrafilter
-pip3 install -r requirements.txt
-echo "Starting extrafilter...."
-python3 main.py
